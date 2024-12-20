a6551a9cc8458a6ec4733ec98e0b4f87819602b0
BenJeMic <FionaSherleen2005@outlook.com>
2024-12-21 00:23:40 +0700

    Updated Init to refresh env

--- init.py
+++ init.py
@@ -8,8 +8,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 #environment
+del os.environ["TOKENA"]
+del os.environ["DISCORD_CLIENT_IDA"]
 load_dotenv(dotenv_path='.env')
 TOKEN = os.getenv('TOKENA')
+logger.info(f"Token: {TOKEN}")
 MAX_CACHE = os.getenv('MAX_CACHE')
 DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_IDA')
+logger.info(f"Client ID: {DISCORD_CLIENT_ID}")
 BOT_INVITE_URL = f"https://discord.com/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=1126967810710593&integration_type=0&scope=applications.commands+bot"
