2e1373838ef9bcc1dbe1e25877b378111d9912db
BenJeMic <FionaSherleen2005@outlook.com>
2024-12-21 00:41:54 +0700

    Fixed last commit again

--- init.py
+++ init.py
@@ -9,7 +9,9 @@ logger = logging.getLogger(__name__)
 
 #environment
 load_dotenv(dotenv_path='.env')
-TOKEN = os.getenv('TOKENA')
+TOKEN = str(os.getenv('TOKENA'))
+logger.info(f"Token: {TOKEN}")
 MAX_CACHE = os.getenv('MAX_CACHE')
 DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_IDA')
+logger.info(f"Client ID: {DISCORD_CLIENT_ID}")
 BOT_INVITE_URL = f"https://discord.com/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=1126967810710593&integration_type=0&scope=applications.commands+bot"
