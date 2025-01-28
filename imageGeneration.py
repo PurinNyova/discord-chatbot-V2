37f7f8a8bc69fedfd926846eaa6f5c0cc8652943
BenJeMic <FionaSherleen2005@outlook.com>
2025-01-28 17:44:02 +0700

    Tried to fix the nsfw thingy again

--- imageGeneration.py
+++ imageGeneration.py
@@ -1,4 +1,5 @@
 from together import Together
+from together import error
 import requests
 import discord
 import asyncio
@@ -8,13 +9,17 @@ from init import IMGGEN
 async def generateImage(prompt: str) -> discord.File:
 
     client = Together(api_key="c1d2906e5b7e4cf216f64952a44088d561c8150f1d9b7b2d7e64a7a177edd37b")
-    response = client.images.generate(
-        prompt=prompt,
-        model="black-forest-labs/FLUX.1-schnell",
-        steps=10,
-        n=1,
-        disable_safety_checker=True,
-    )
+    try:
+        response = client.images.generate(
+            prompt=prompt,
+            model="black-forest-labs/FLUX.1-schnell-Free",
+            steps=10,
+            n=1,
+            disable_safety_checker=True,
+        )
+    except error:
+        return error
+    
     print("image sec")
     res = requests.get(response.data[0].url)
     fileObj = BytesIO(res.content)
